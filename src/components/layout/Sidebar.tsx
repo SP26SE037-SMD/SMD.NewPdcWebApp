4c7d641a917de660771ebabe60df678706ae0e1b
troidatoi <minhtqse184747@fpt.edu.vn>
2026-04-29 10:17:18 +0700

    Adjust Sidebar padding to pb-20

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -59,7 +59,7 @@ export function Sidebar({
                 </div>
             )}
 
-            <nav className="flex-1 px-3 flex flex-col justify-center space-y-2 pb-24">
+            <nav className="flex-1 px-3 flex flex-col justify-center space-y-2 pb-20">
                 {items.filter(item => !item.isAction).map((item) => (
                     <button
                         key={item.id}
