0eb7cd40ae9e4844a233616fc38a63b89dba4cc9
troidatoi <minhtqse184747@fpt.edu.vn>
2026-05-07 08:54:56 +0700

    chore: remove old test files

--- test-api.js
+++
@@ -1,8 +0,0 @@
-const fetch = require('node-fetch');
-async function run() {
-  const res = await fetch('http://43.207.156.116/api/sessions/validate', { method: 'POST', body: '[]', headers: { 'Content-Type': 'application/json' } });
-  const text = await res.text();
-  console.log("STATUS:", res.status);
-  console.log("BODY:", text);
-}
-run();
